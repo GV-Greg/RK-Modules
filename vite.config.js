cd2868337d16d9fe1bd8da32f9d2357ffe47ede4
Greg GV <gregvandenbergh@gmail.com>
2022-04-04 00:26:54 +0200

    Installation vite-plugin-gh-pages
    for automatic deploy on gh-pages when build run

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { ghPages } from "vite-plugin-gh-pages"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/RK-Modules/',
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), ghPages()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
